2d1bd0112a57c336189a996af3e5a8247c281f12
marichemia <91846117+marichemia@users.noreply.github.com>
2023-07-18 22:42:27 +0400

    Create homework-3.js

---
+++ homework-3.js
@@ -0,0 +1,22 @@
+/* Arrays task 1 */
+
+
+function f(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("invalid input, parameter must be an array");
+    }
+    let sum = 0;
+    function add(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                add(arr[i]);
+            } else if (typeof arr[i] === "number") {
+                sum += arr[i];
+            } else {
+                throw new Error("Array must contain only numbers or arrays");
+            }
+        }
+    }
+    add(arr);
+    return sum;
+}
